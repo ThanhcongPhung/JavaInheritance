400337e62c51a5300191466cc2b5b4f8b3b0242a
ThanhcongPhung <43209065+ThanhcongPhung@users.noreply.github.com>
2018-11-08 13:34:32 +0700

    Edit funtion xoaConVat

--- src/Cau2/Chuong.java
+++ src/Cau2/Chuong.java
@@ -18,12 +18,17 @@ public class Chuong {
     }
 
     public void xoaConVat(String ten) {
+        boolean search = false;
         for (Animal animal : AnimalLists) {
             if (animal.Ten.equals(ten)) {
                 AnimalLists.remove(ten);
                 System.out.printf("Da xoa con vat %s\n", ten);
+                search = true;
                 break;
             }
+        }
+        if (search == false) {
+            System.out.printf("Khong tim thay con vat %s", ten);
         }
     }
     public void xemConVat(){
